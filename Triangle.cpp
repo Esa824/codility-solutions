e8f402f0c668c912dee8472abe757c15aab7811d
esaslaga <esa.slaga@gmail.com>
2022-05-11 14:52:14 +0500

    added another soultion

---
+++ Triangle.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int Tringle(vector<int> &A) {
+  vector<long> arr(A.begin(), A.end());
+  sort(arr.begin(), arr.end());
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] + arr[i + 1] > arr[i + 2] && arr[i + 1] + arr[i + 2] > arr[i] &&
+        arr[i + 2] + arr[i] > arr[i + 1]) {
+      return 1;
+    }
+    if (i + 3 >= arr.size()) {
+      break;
+    }
+  }
+  return 0;
+}
